eaafd80a5d45794e3cb400fe179e3c94213e9b55
Shuhei Nishimura <nishimura@digitalcube.jp>
2016-02-28 10:57:59 +0900

    first commit

---
+++ ec2-nginx.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sudo /usr/sbin/usermod -s /bin/bash nginx;
+sudo passwd nginx;
+sudo /sbin/service monit stop;
+sudo /sbin/service nginx stop;
+sudo /sbin/service php-fpm stop;
+sudo /usr/sbin/usermod -d /var/www/vhosts nginx;
+sudo /sbin/service php-fpm start;
+sudo /sbin/service nginx start;
+sudo /sbin/service monit start;
+
+sudo mkdir /var/www/vhosts/.ssh;
+sudo cp /home/ec2-user/.ssh/authorized_keys /var/www/vhosts/.ssh/;
+sudo chown -R nginx:nginx /var/www/vhosts/.ssh/;
